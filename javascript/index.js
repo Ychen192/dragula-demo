699eae3ea7af44d8fabd7ecb1b8e93f0e9dd4198
yachen <ychen192>
2019-08-28 17:34:35 -0400

    added multiple target containers

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,7 @@ function $(id) {
   return document.getElementById(id);
 }
 
-dragula([$('drag-elements'), $('drop-target')], {
+dragula([$('drag-elements'), $('drop-target'), $('drop-target1'), $('drop-target2')], {
   copy: function (el, source) {
     return source === $('drag-elements');
   },
